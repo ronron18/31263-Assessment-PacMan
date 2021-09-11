9e9eccf7ccc54030d8cbecef53de3fd519725c98
ronron18 <ronankonstantin@gmail.com>
2021-09-11 20:09:51 +1000

    Completed audio section

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    Controls audio and stuff
+*/
+
+
+public class AudioController : MonoBehaviour
+{
+    public AudioClip startBGM;
+    public AudioClip normalBGM;
+    public AudioClip scaredBGM;
+    public AudioClip oneEatenBGM;
+
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource.PlayOneShot(startBGM);
+        audioSource.clip = normalBGM;
+        audioSource.loop = true;
+        audioSource.PlayDelayed(startBGM.length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ChangeClip(AudioClip clip)
+    {
+        audioSource.Stop();
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+}
