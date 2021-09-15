11c56791cac0b82d18540e41ddb32df74eed7d2e
ronron18 <ronankonstantin@gmail.com>
2021-09-15 20:45:23 +1000

    Working on player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Vector3[] positions;
+    public int direction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        positions = new Vector3[4];
+        positions[0] = new Vector3(-9.0f, 5.0f, 0.0f);
+        positions[1] = new Vector3(-4.0f, 5.0f, 0.0f);
+        positions[2] = new Vector3(-4.0f, 1.0f, 0.0f);
+        positions[3] = new Vector3(-9.0f, 1.0f, 0.0f);
     }
 
     // Update is called once per frame
